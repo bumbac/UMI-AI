bd60047d1097dfcf4c73965940d59d93cf36db51
sutymate <sutymate@fit.cvut.cz>
2021-10-06 12:16:39 +0200

    finish MST, EUL, HAM

--- 1/main.py
+++ 1/main.py
@@ -12,58 +12,129 @@ class Node:
         self.id = ID
 
 
-def printroom(room):
-    for y_line in room:
-        for node in y_line:
-            print(node.symbol, end='')
-        print('\n')
-
-
-def adjacency_matrix(room):
-    x_lim = len(room[0])
-    y_lim = len(room)
-    matrix = [[[] for i in range(x_lim)] for j in range(y_lim)]
-    for y_line in room:
-        for node in y_line:
-            if node.symbol == 'x':
-                continue
-            for x_offset in [1, 0, -1]:
-                for y_offset in [1, 0, -1]:
-                    if x_offset*y_offset == 0:
-                        x_curr = node.x + x_offset
-                        y_curr = node.y + y_offset
-                        if x_lim > x_curr >= 0 and 0 <= y_curr < y_lim:
-                            if room[y_curr][x_curr].symbol != 'x':
-                                matrix[node.y][node.x].append(room[y_curr][x_curr])
-    return matrix
-
-
-def mst(room, dust_locations, robot_location):
-    st = []
-    bfs_tree = bfs(copy.deepcopy(room), robot_location, spanning_tree=True)
-    rep = 1
-    for dust in dust_locations:
-        st.append([0, bfs_tree[dust[1]][dust[0]]])
-        node = st[-1][1]
-        price = 0
-        while node:
-            price += 1
-            if node.symbol == 'O':
-                print("Found start.")
+def hamiltonian(vertices, double_edges):
+    guide = {}
+    node_pos = 0
+    for v in vertices:
+        guide[v.id] = node_pos
+        node_pos += 1
+    node_pos = 0
+    walk = []
+    while len(double_edges[node_pos]) != 0:
+        next_node = double_edges[node_pos][0]
+        for next_edge in double_edges[node_pos]:
+            if double_edges[node_pos].count(next_edge) == 2:
+                next_node = next_edge
                 break
-            node.symbol = str(rep)
-            node = node.prev
-        rep += 1
-        st[-1][0] = price
-    printroom(bfs_tree)
-    sorted_st = sorted(st, key=lambda x: x[0])
+        reverse_node_pos = guide[next_node.id]
+        this_node = vertices[node_pos]
+        double_edges[node_pos].remove(next_node)
+        double_edges[reverse_node_pos].remove(this_node)
+        walk.append(vertices[node_pos])
+        print(walk)
+        node_pos = guide[next_node.id]
+    # shortcut
+    visited = []
+    pos = 0
+    limit = len(walk)
+    while pos < limit:
+        element = walk[pos]
+        if element not in visited:
+            visited.append(element)
+        pos += 1
+    return visited
 
 
-    adj_matrix = adjacency_matrix(room)
+def eulerian(vertices):
+    double_edges = [[] for i in range((len(vertices)))]
+    node_pos = 0
+    for vortex in vertices:
+        prev = vortex.prev
+        if prev:
+            double_edges[node_pos].append(prev)
+            double_edges[node_pos].append(prev)
+            idx = vertices.index(prev)
+            double_edges[idx].append(vortex)
+            double_edges[idx].append(vortex)
+        node_pos += 1
+    return double_edges
+
+
+def edge_price(start, end, room):
+    return bfs(room, (start.x, start.y), spanning_tree=True, ID=end.id)
+
+
+def mst(room, dust_nodes, robot_location):
+    printroom(room)
+    edges = [[99999 for i in range(len(dust_nodes) + 1)] for i in range((len(dust_nodes) + 1))]
+    vertices = [room[robot_location[1]][robot_location[0]]]
+    vertices.extend(dust_nodes)
+    y = 0
+    for start in vertices:
+        x = 0
+        for end in vertices:
+            if start == end:
+                x += 1
+                continue
+            edges[y][x] = edge_price(start, end, copy.deepcopy(room))
+            x += 1
+        y += 1
+    for node in vertices:
+        print(node.id, node.symbol)
+    node = vertices[0]
+    node_pos = 0
+    free_nodes = [i for i in range(len(vertices))]
+    free_nodes.remove(node_pos)
+    while free_nodes:
+        next_pos = edges[node_pos].index(min(edges[node_pos]))
+        edges[node_pos][next_pos] = 99999
+        if next_pos not in free_nodes:
+            continue
+        vertices[next_pos].prev = node
+        node = vertices[next_pos]
+        node_pos = next_pos
+        free_nodes.remove(node_pos)
+    return vertices
+
+# MST: first create minimum spanning tree of nodes: robot_location and all dust_nodes
+# graph is complete Kn graph, where distance is computed using BFS between nodes(robot and dust nodes)
+
+# EULERIAN: double every edge between nodes and create Eulerian walk = sequence of nodes which can be visited > 1
+
+# HAMILTIONIAN: because all dust nodes and robot nodes are accessible, create shortcuts by leaving out
+# already accessed nodes (from 1-2-2-3-4-5-5-6 create 1-2-3-4-5-6)
+
+# this sequence is proposed order of visit between nodes,
+def vacuum_smart(room, dust_nodes, robot_location):
+    vertices = mst(room, dust_nodes, robot_location)
+    print("MST")
+    for v in vertices:
+        print(v.id)
+        prev = v.prev
+        while prev:
+            print(prev.id)
+            prev = prev.prev
+        print()
+    double_edges = eulerian(vertices)
+    print("EULERIAN WALK")
+    pos = 0
+    for e in double_edges:
+        print("FROM", vertices[pos].id)
+        pos += 1
+        for node in e:
+            print(node.id)
+        print()
+    print("HAMILTONIAN")
+    walk = hamiltonian(vertices, double_edges)
+    for w in walk:
+        print(w.id, end='->')
+    
+
 
 
 def task1(room):
     dust_locations = []
+    dust_nodes = []
     robot_location = (0, 0)
     y = 0
     for y_line in room:
@@ -73,17 +144,20 @@ def task1(room):
                 robot_location = (x, y)
             if node.symbol == '@':
                 dust_locations.append((x, y))
+                dust_nodes.append(node)
             x += 1
         y += 1
-    start_time = time.time_ns()
-    solution_bfs = vacuum_bfs(copy.deepcopy(room), robot_location)
-    benchmark = time.time_ns() - start_time
-    print("BENCHMARK BFS", benchmark)
-    printroom(solution_bfs)
-    solution_mst = mst(room, dust_locations, robot_location)
+    # start_time = time.time_ns()
+    # solution_bfs = vacuum_bfs(copy.deepcopy(room), robot_location)
+    # benchmark = time.time_ns() - start_time
+    # print("BENCHMARK BFS", benchmark)
+    # printroom(solution_bfs)
+    #solution_mst = mst(room, dust_locations, robot_location)
+    vacuum_smart(room, dust_nodes, robot_location)
 
 
-def bfs(room, robot_location, spanning_tree=False):
+
+def bfs(room, robot_location, spanning_tree=False, ID=None):
     start = room[robot_location[1]][robot_location[0]]
     x_lim = len(room[0])
     y_lim = len(room)
@@ -94,6 +168,12 @@ def bfs(room, robot_location, spanning_tree=False):
         node = queue.pop(0)
         if node.flag == 1:
             node.flag = 2
+            if node.id == ID:
+                price = 0
+                while node:
+                    node = node.prev
+                    price += 1
+                return price
             if node.symbol == '@':
                 if not spanning_tree:
                     return node
@@ -133,6 +213,13 @@ def vacuum_bfs(room, robot_location):
         rep = chr(ord(rep) + 1)
         position += 1
     return room
+
+
+def printroom(room):
+    for y_line in room:
+        for node in y_line:
+            print(node.symbol, end='')
+        print('\n')
 
 
 def readfile(filename):
