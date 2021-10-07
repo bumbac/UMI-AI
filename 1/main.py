b1e074eef1103633236d2d07aefdfb3949c622b8
sutymate <sutymate@fit.cvut.cz>
2021-10-07 10:45:21 +0200

    add comments, fix mst

--- 1/main.py
+++ 1/main.py
@@ -1,5 +1,4 @@
 import copy
-import time
 
 
 class Node:
@@ -13,6 +12,9 @@ class Node:
 
 
 def hamiltonian(vertices, double_edges):
+    """
+    visit each node only once by creating shortcuts between nodes
+    """
     guide = {}
     node_pos = 0
     for v in vertices:
@@ -32,6 +34,7 @@ def hamiltonian(vertices, double_edges):
         double_edges[reverse_node_pos].remove(this_node)
         walk.append(vertices[node_pos])
         node_pos = guide[next_node.id]
+    print("\nSMART VACUUM SEQUENCE OF STEPS")
     # shortcut
     visited = []
     pos = 0
@@ -40,12 +43,17 @@ def hamiltonian(vertices, double_edges):
         element = walk[pos]
         if element not in visited:
             visited.append(element)
-            # print(element.id, end='->')
+            print(element.id, end='->')
         pos += 1
+    print('FINISH')
     return visited
 
 
 def eulerian(vertices):
+    """
+    Add extra edge between nodes connected in MST
+    visit each edge once, create walk
+    """
     double_edges = [[] for i in range((len(vertices)))]
     node_pos = 0
     for vortex in vertices:
@@ -65,34 +73,45 @@ def edge_price(start, end, room):
 
 
 def mst(room, dust_nodes, robot_location):
-    edges = [[99999 for i in range(len(dust_nodes) + 1)] for i in range((len(dust_nodes) + 1))]
+    """
+    create complete graph of vertices from dust_nodes and robot location
+    calculate price of every edge between vertices
+    create minimal spanning tree by hungry algorithm by assigning previous
+    node to each node
+    """
     vertices = [room[robot_location[1]][robot_location[0]]]
     vertices.extend(dust_nodes)
-    y = 0
+    min_edges = []
+    aux_vertices = copy.deepcopy(vertices)
     for start in vertices:
-        x = 0
-        for end in vertices:
-            if start == end:
-                x += 1
+        for end in aux_vertices:
+            if start.id == end.id:
                 continue
-            edges[y][x] = edge_price(start, end, copy.deepcopy(room))
-            x += 1
-        y += 1
-    # for node in vertices:
-    #     print(node.id, node.symbol)
-    node = vertices[0]
-    node_pos = 0
-    free_nodes = [i for i in range(len(vertices))]
-    free_nodes.remove(node_pos)
-    while free_nodes:
-        next_pos = edges[node_pos].index(min(edges[node_pos]))
-        edges[node_pos][next_pos] = 99999
-        if next_pos not in free_nodes:
-            continue
-        vertices[next_pos].prev = node
-        node = vertices[next_pos]
-        node_pos = next_pos
-        free_nodes.remove(node_pos)
+            price = edge_price(start, end, copy.deepcopy(room))
+            min_edges.append([price, start, end])
+        aux_vertices.pop(0)
+    min_edges.sort(key=lambda x: x[0], reverse=True)
+    free_nodes = []
+    guide = {}
+    i = 0
+    used_nodes = []
+    for v in vertices:
+        free_nodes.append(v.id)
+        guide[v.id] = i
+        i += 1
+    m = []
+    while len(min_edges) > 0:
+        last = min_edges.pop()
+        flag = False
+        if last[1].id not in used_nodes:
+            used_nodes.append(last[1].id)
+            flag = True
+        if flag:
+            last[2].prev = last[1]
+            m.append((last[1], last[2]))
+            prev_pos = guide[last[1].id]
+            pos = guide[last[2].id]
+            vertices[prev_pos].prev = vertices[pos]
     return vertices
 
 # MST: first create minimum spanning tree of nodes: robot_location and all dust_nodes
@@ -103,48 +122,42 @@ def mst(room, dust_nodes, robot_location):
 # HAMILTIONIAN: because all dust nodes and robot nodes are accessible, create shortcuts by leaving out
 # already accessed nodes (from 1-2-2-3-4-5-5-6 create 1-2-3-4-5-6)
 
-# this sequence is proposed order of visit between nodes,
+# this sequence is proposed order of visit between nodes
 def vacuum_smart(room, dust_nodes, robot_location):
+    # MST
     vertices = mst(room, dust_nodes, robot_location)
-    print("MST")
-    for v in vertices:
-        # print(v.id)
-        prev = v.prev
-        while prev:
-            # print(prev.id)
-            prev = prev.prev
-        # print()
+    # EULERIAN
     double_edges = eulerian(vertices)
-    print("EULERIAN WALK")
-    pos = 0
-    for e in double_edges:
-        # print("FROM", vertices[pos].id)
-        pos += 1
-        # for node in e:
-            # print(node.id)
-        # print()
-    print("HAMILTONIAN")
+    # HAMILTONIAN
     walk = hamiltonian(vertices, double_edges)
     steps = 0
+    # clean node.prev values for calculating steps
     for yline in room:
         for v in yline:
             if v.flag != 0:
                 print(v.id, "not zero")
             if v.prev:
                 v.prev = None
+    # calculate price of tour for collecting all dust
+
     for i in range(len(walk) - 1):
         steps += edge_price(walk[i], walk[i+1], copy.deepcopy(room))
+    print("SMART VACUUM:", steps, "steps.")
     return steps
 
 
-
-
 def task1(room):
+    """
+    Vacuum cleaner, find route to collect all dust (symbol @)
+    Robot starts at position with symbol big O
+    Symbol x is wall, other is accessible
+    """
     printroom(room)
     dust_locations = []
     dust_nodes = []
     robot_location = (0, 0)
     y = 0
+    # find locations of all dusts
     for y_line in room:
         x = 0
         for node in y_line:
@@ -156,11 +169,13 @@ def task1(room):
             x += 1
         y += 1
     solution_bfs = vacuum_bfs(copy.deepcopy(room), robot_location)
-    steps = vacuum_smart(room, dust_nodes, robot_location)
-    print("SMART VACUUM:", steps)
+    solutin_smart = vacuum_smart(room, dust_nodes, robot_location)
 
 
 def bfs(room, robot_location, spanning_tree=False, ID=None):
+    """
+    Using bfs find dust (or node with ID).
+    """
     start = room[robot_location[1]][robot_location[0]]
     x_lim = len(room[0])
     y_lim = len(room)
@@ -199,22 +214,33 @@ def bfs(room, robot_location, spanning_tree=False, ID=None):
 
 
 def vacuum_bfs(room, robot_location):
+    """
+    Naive solution - find dust closest to robot.
+    Collect it and find another closest to current location.
+    All using BFS.
+    """
+    start_location = room[robot_location[1]][robot_location[0]]
     dust = bfs(copy.deepcopy(room), robot_location)
     moves = []
-    print(robot_location)
+    print('Robot location:', robot_location)
     while dust:
-        print(dust.x, dust.y)
         robot_location = (dust.x, dust.y)
         room[dust.y][dust.x].symbol = '.'
         while dust.prev:
-            dust.symbol = '.'
-            room[dust.y][dust.x].symbol = '.'
             moves.append(dust)
             dust = dust.prev
-        moves.append(dust)
-        print(len(moves))
         dust = bfs(copy.deepcopy(room), robot_location)
-    print("BFS:", len(moves))
+    print("\nBFS:", len(moves), "steps.")
+    print("SEQUENCE OF DUSTS")
+    start_location.symbol = 'O'
+    moves.insert(0, start_location)
+    visited = []
+    for node in moves:
+        if node.symbol == '@' or node.symbol =='O':
+            if node.id not in visited:
+                print(node.id, end='->')
+                visited.append(node.id)
+    print("FINISH")
     return room
 
 
